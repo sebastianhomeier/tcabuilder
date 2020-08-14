0e404e92698414577b75a1fb73d53db0a4aca031
Thomas Löffler <loeffler@spooner-web.de>
2020-08-14 23:26:01 +0200

    Add zip extension and unzip package

--- docker_install.sh
+++ docker_install.sh
@@ -5,15 +5,12 @@
 
 set -xe
 
-# Install git (the php image doesn't have it) which is required by composer
+# Install git (the php image doesn't have it) which is required by composer, and zip
 apt-get update -yqq
-apt-get install git -yqq
+apt-get install git unzip -yqq
+docker-php-ext-install zip
 
-# Install phpunit, the tool that we will use for testing
-curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-chmod +x /usr/local/bin/phpunit
-
-# Install xdehug
+# Install xdebug
 pecl install xdebug
 docker-php-ext-enable xdebug
 

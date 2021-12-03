6a364bd12de44ed4fa203a3dc7952fbf423ac738
Thomas Löffler <loeffler@spooner-web.de>
2021-12-03 11:06:17 +0100

    [TASK] Add possibility to run TYPO3 in dev and change project

--- .gitlab-ci/rector.php
+++ .gitlab-ci/rector.php
@@ -3,25 +3,18 @@ declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
-use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\PostRector\Rector\NameImportingPostRector;
 use Ssch\TYPO3Rector\Configuration\Typo3Option;
 use Ssch\TYPO3Rector\FileProcessor\Composer\Rector\ExtensionComposerRector;
-use Ssch\TYPO3Rector\FileProcessor\TypoScript\Rector\FileIncludeToImportStatementTypoScriptRector;
-use Ssch\TYPO3Rector\Rector\v9\v0\InjectAnnotationRector;
-use Ssch\TYPO3Rector\Rector\General\ConvertTypo3ConfVarsRector;
+use Ssch\TYPO3Rector\Rector\General\ConvertImplicitVariablesToExplicitGlobalsRector;
 use Ssch\TYPO3Rector\Rector\General\ExtEmConfRector;
-use Ssch\TYPO3Rector\Set\Typo3SetList;
+use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
-    $containerConfigurator->import(Typo3SetList::TYPO3_76);
-    $containerConfigurator->import(Typo3SetList::TYPO3_87);
-    $containerConfigurator->import(Typo3SetList::TYPO3_95);
-    $containerConfigurator->import(Typo3SetList::TYPO3_104);
-    $containerConfigurator->import(Typo3SetList::TYPO3_11);
+    $containerConfigurator->import(Typo3LevelSetList::UP_TO_TYPO3_11);
 
     // In order to have a better analysis from phpstan we teach it here some more things
     $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, Typo3Option::PHPSTAN_FOR_RECTOR_PATH);
@@ -106,7 +99,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // The filename can be passed as argument, "Configuration_Extbase_Persistence_Classes.php" is default.
     // $services->set(ExtbasePersistenceTypoScriptRector::class);
     // Add some general TYPO3 rules
-    $services->set(ConvertTypo3ConfVarsRector::class);
+    $services->set(ConvertImplicitVariablesToExplicitGlobalsRector::class);
     $services->set(ExtEmConfRector::class);
     $services->set(ExtensionComposerRector::class);
 

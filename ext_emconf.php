131af8ae35e1ad1357e4f07c6d02a506302d8d1c
Thomas Löffler <loeffler@spooner-web.de>
2023-05-17 15:07:00 +0200

    [RELEASE] Version 3.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'TCA Builder - create and change TCA in an easy way',
     'description' => 'Utility to easily maintain and create your TCA forms',
-    'version' => '3.0.0',
+    'version' => '3.1.0',
     'category' => 'misc',
     'state' => 'stable',
     'author' => 'Thomas Löffler',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => true,
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0 - 12.9.99',
+            'typo3' => '11.5.0 - 12.4.99',
         ],
     ],
 ];
